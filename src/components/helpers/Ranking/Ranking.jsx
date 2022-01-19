f170fbed8e54a7870a9847546d15b0ef22907216
unknown <chri779r@stud.kea.dk>
2022-01-19 10:09:08 +0100

    data charts work

--- src/components/helpers/Ranking/Ranking.jsx
+++ src/components/helpers/Ranking/Ranking.jsx
@@ -1,15 +1,50 @@
+import React, { useRef, useEffect } from "react";
 import "./Ranking.scss"
+
+import Chart from 'chart.js/auto';
+
 
 
 export default function TopBeer(props) {
     const ranking = [...props.storage];
-    let rankingSort = ranking.sort((a, b)=>a.amount-b.amount);
-    const rankingMap = rankingSort.map((keg, i ) => <li key={i}>{keg.name}</li> );
+    // let rankingSort = ranking.sort((a, b)=>a.amount-b.amount); FJERNET FOR VIASUAL
+    // const rankingMap = rankingSort.map((keg, i ) => <li key={i}>{keg.name}</li> ); BRUGES IKKE
+
+    const ordersChartRef = useRef()
+    useEffect(() =>{ 
+      if (!ordersChartRef.current) {
+        // Stopper execution, hvis current ikke er sat
+        return
+      }
+  
+      console.log(ordersChartRef.current);
+      const myChart = new Chart(ordersChartRef.current, {
+        type: 'bar',
+        data: {
+            labels: ranking.map(beer => beer.name),
+              datasets: [{
+                label: 'My First Dataset',
+                data: ranking.map(beer => beer.amount),
+                backgroundColor: [
+                  'rgb(255, 99, 132)',
+                  'rgb(75, 192, 192)',
+                  'rgb(255, 205, 86)',
+                  'rgb(201, 203, 207)',
+                  'rgb(54, 162, 235)'
+                ]
+              }]
+        }
+    });
+    return () => {
+        //NÅR DATA SKIFTER SKAL DEN "FJERNE" CHARTEN OG LAVE EN NY
+      myChart.destroy();
+    }
+    },[ordersChartRef.current, ranking.length]) 
 
     return (
         <div className="topBeer">
             <h3>Top Sellers</h3>
-            <ul>{rankingMap}</ul>
+            <canvas ref={ordersChartRef} width="150" height="150"></canvas>
         </div>
     )
 
--- src/components/helpers/Sales/Sales.jsx
+++ src/components/helpers/Sales/Sales.jsx
@@ -1,27 +1,104 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./Sales.scss";
+import Chart from 'chart.js/auto';
+
 
 export default function Sales(props) {
-    if (!props.products) {
-        return null;
-      }
-    const namePrice = props.products.map((beer) => {
-        const name = beer.name.toLowerCase().split(" ").join("");
-        const total = props.ranking[name] * beer.price();
-        return total;
-      });
     
-      const initialValue = 0;
-      let sum = namePrice.reduce(function (previousValue, currentValue) {
-        return previousValue + currentValue;
-      }, initialValue);
+  if (!props.products) {
+    return null;
+  }
+
+const namePrice = props.products.map((beer) => {
+    const name = beer.name.toLowerCase().split(" ").join("");
+    const total = props.ranking[name] * beer.price();
+    return total;
+  });
+
+  const initialValue = 0;
+  let sum = namePrice.reduce(function (previousValue, currentValue) {
+    return previousValue + currentValue;
+  }, initialValue);
+
+  const totalChartRef = useRef()
+  useEffect(() =>{ 
+    if (!totalChartRef.current) {
+      // Stopper execution, hvis current ikke er sat
+      return
+    }
+
+    const total = 13000
+    console.log(totalChartRef.current);
+    const myChart = new Chart(totalChartRef.current, {
+      type: 'doughnut',
+      data: {
+        labels: [
+          'Todays Income',
+          'Todays Goal',
+        ],
+        datasets: [{
+          label: 'Income chart',
+          data: [sum, total-sum],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)'
+          ],
+          hoverOffset: 4
+        }]
+      }
+  });
+  return () => {
+    myChart.destroy();
+  }
+  },[totalChartRef.current, sum]) 
+
+//---------------------------------//
+  const ordersChartRef = useRef()
+  useEffect(() =>{ 
+    if (!ordersChartRef.current) {
+      // Stopper execution, hvis current ikke er sat
+      return
+    }
+
+    const total = 100
+    console.log(ordersChartRef.current);
+    const myChart = new Chart(ordersChartRef.current, {
+      type: 'doughnut',
+      data: {
+        labels: [
+          'Number of orders',
+          'Todays Goal',
+        ],
+        datasets: [{
+          label: 'Orders Chart',
+          data: [props.dayOrders, total-props.dayOrders],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)'
+          ],
+          hoverOffset: 4
+        }]
+      }
+  });
+  return () => {
+    myChart.destroy();
+  }
+  },[ordersChartRef.current, props.dayOrders]) 
+
+  
     
       console.log(namePrice);
       console.log(sum);
 
 
     return (
-        <div className="sales">
+<>
+<div>
+<canvas ref={totalChartRef} width="400" height="400"></canvas>
+<canvas ref={ordersChartRef} width="400" height="400"></canvas>
+</div>
+
+<div className="sales">
             <div className="salesHeader">
               <h3>Todays sales</h3>
           </div>
@@ -40,5 +117,8 @@ export default function Sales(props) {
             </div>
           </div>
         </div>
+</>
+
+        
     )
 }
